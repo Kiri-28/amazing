dd8ba1447585c36b16d457e1c4b67d688a61f193
Aingeru Sanchez <aingerusanchez72@gmail.com>
2022-06-07 00:52:45 +0200

    feat(): add basic example

---
+++ models/product.model.js
@@ -0,0 +1,12 @@
+export class Product {
+    /* Product unique identifier */
+    id;
+    /* Product name */
+    name;
+    /* Product short description */
+    description;
+    /* Product price */
+    price;
+    /* (Optional) Product image src url */
+    imageUrl;
+}
